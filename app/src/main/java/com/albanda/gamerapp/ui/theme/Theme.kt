8b7e9c83baa711bcb5f81aeb7fb3f40840b58faa
Albert <afiguerola@gaddexapp.com>
2025-05-02 08:48:31 +0200

    Fixed code to correctly display colors associated with dark mode and light mode.
    The gray colors have been renamed.

--- app/src/main/java/com/albanda/gamerapp/ui/theme/Theme.kt
+++ app/src/main/java/com/albanda/gamerapp/ui/theme/Theme.kt
@@ -1,28 +1,25 @@
 package com.albanda.gamerapp.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Red500,
     onPrimary = Color.White,
-    primaryContainer = Darkgray900,
-    secondary = Darkgray700,
-    background = Darkgray900
+    primaryContainer = DarkGray900,
+    secondary = DarkGray700,
+    background = DarkGray900
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    onBackground = Color.Red,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -42,7 +39,9 @@ fun GamerAppTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    /*val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -50,7 +49,7 @@ fun GamerAppTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
+    }*/
 
     MaterialTheme(
         colorScheme = colorScheme,
