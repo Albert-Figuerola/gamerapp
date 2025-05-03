0c5f77293947e13abf40c95228b7d0e9793660e5
Albert <afiguerola@gaddexapp.com>
2025-05-03 16:23:27 +0200

    Added the "visibility" and "visibility_off" icons, as well as the ability to show or hide the password.

--- app/src/main/java/com/albanda/gamerapp/screens/login/components/LoginContent.kt
+++ app/src/main/java/com/albanda/gamerapp/screens/login/components/LoginContent.kt
@@ -16,11 +16,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -54,10 +59,6 @@ fun LoginContent() {
         Spacer(modifier = Modifier.height(32.dp))
 
         CardForm()
-
-        //Spacer(modifier = Modifier.height(32.dp))
-
-
     }
 }
 
@@ -83,9 +84,6 @@ fun CardForm() {
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Bold
             )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
             Text(
                 text = "Por favor, inicia sesión para continuar.",
                 fontSize = 12.sp,
@@ -106,21 +104,9 @@ fun CardForm() {
                     )
                 }
             )
-            OutlinedTextField(
-                modifier = Modifier.padding(top = 5.dp),
-                value = "",
-                onValueChange = {},
-                label = {
-                    Text("Contraseña")
-                },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Default.Lock,
-                        contentDescription = "",
-                        tint = Color.White
-                    )
-                }
-            )
+
+            PasswordTextField()
+
             Button(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -139,6 +125,43 @@ fun CardForm() {
             }
         }
     }
+}
+
+@Composable
+fun PasswordTextField() {
+    //var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = Modifier.padding(top = 5.dp),
+        value = "",
+        onValueChange = {},
+        label = {
+            Text("Contraseña")
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Lock,
+                contentDescription = "",
+                tint = Color.White
+            )
+        },
+        trailingIcon = {
+            val image = if (passwordVisible) {
+                R.drawable.visibility
+            } else {
+                R.drawable.visibility_off
+            }
+
+            IconButton(onClick = { passwordVisible = !passwordVisible } ) {
+                Icon(
+                    painter = painterResource(id = image),
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
