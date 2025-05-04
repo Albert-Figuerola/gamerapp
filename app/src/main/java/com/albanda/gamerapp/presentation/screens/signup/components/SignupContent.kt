e1681cf6fd2af030ce22903a1c515d31d862afd0
Albert <afiguerola@gaddexapp.com>
2025-05-04 21:05:00 +0200

    The registration form is complete.

--- app/src/main/java/com/albanda/gamerapp/presentation/screens/signup/components/SignupContent.kt
+++ app/src/main/java/com/albanda/gamerapp/presentation/screens/signup/components/SignupContent.kt
@@ -1,48 +1,20 @@
 package com.albanda.gamerapp.presentation.screens.signup.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.outlined.Lock
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.albanda.gamerapp.R
-import com.albanda.gamerapp.presentation.components.DefaultButton
-import com.albanda.gamerapp.presentation.components.DefaultTextFiled
-import com.albanda.gamerapp.presentation.ui.theme.DarkGray500
 import com.albanda.gamerapp.presentation.ui.theme.GamerAppTheme
-import com.albanda.gamerapp.presentation.ui.theme.Red500
 
 @Composable
 fun SignupContent() {
@@ -60,139 +32,6 @@ fun SignupContent() {
         Spacer(modifier = Modifier.height(32.dp))
     }
 
-}
-
-@Composable
-fun BoxHeader() {
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .height(250.dp)
-        .background(Red500)
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(Modifier.height(64.dp))
-            Image(
-                modifier = Modifier.height(120.dp),
-                painter = painterResource(id = R.drawable.user),
-                contentDescription = "User image"
-            )
-        }
-    }
-}
-
-@Composable
-fun CardForm() {
-    var username by remember {
-        mutableStateOf("")
-    }
-
-    var email by remember {
-        mutableStateOf("")
-    }
-
-    var password by remember {
-        mutableStateOf("")
-    }
-
-    var confirmPassword by remember {
-        mutableStateOf("")
-    }
-
-    Card(
-        modifier = Modifier
-            .padding(start = 40.dp, end = 40.dp, top = 200.dp),
-        colors = CardDefaults.cardColors(containerColor = DarkGray500)
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(horizontal = 20.dp)
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(
-                        top = 32.dp,
-                        bottom = 0.dp,
-                        start = 0.dp,
-                        end = 0.dp
-                    ),
-                text = "REGISTRO",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = "Por favor ingresa estos datos para continuar.",
-                fontSize = 12.sp,
-                color = Color.Gray
-            )
-
-            DefaultTextFiled(
-                modifier = Modifier.padding(top = 16.dp),
-                value = username,
-                onValueChange = { username = it },
-                label = "Nombre de usuario",
-                leadingIcon = Icons.Default.Person
-            )
-            
-            DefaultTextFiled(
-                modifier = Modifier.padding(top = 0.dp),
-                value = email,
-                onValueChange = { email = it },
-                label = "Correo electrónico",
-                leadingIcon = Icons.Default.Email,
-                keyboardType = KeyboardType.Email
-            )
-
-            var hideText by remember { mutableStateOf(true) }
-            val trailing: @Composable () -> Unit = {
-                val image = if (hideText) {
-                    R.drawable.visibility_off
-                } else {
-                    R.drawable.visibility
-                }
-
-                IconButton(onClick = { hideText = !hideText } ) {
-                    Icon(
-                        painter = painterResource(id = image),
-                        contentDescription = "",
-                        tint = Color.White
-                    )
-                }
-            }
-
-            DefaultTextFiled(
-                modifier = Modifier.padding(top = 0.dp),
-                value = password,
-                onValueChange = { password = it },
-                label = "Password",
-                leadingIcon = Icons.Default.Lock,
-                trailingIcon = trailing,
-                hideText = hideText,
-                keyboardType = KeyboardType.Password
-            )
-
-            DefaultTextFiled(
-                modifier = Modifier.padding(top = 0.dp),
-                value = confirmPassword,
-                onValueChange = { confirmPassword = it },
-                label = "Password",
-                leadingIcon = Icons.Outlined.Lock,
-                trailingIcon = trailing,
-                hideText = hideText,
-                keyboardType = KeyboardType.Password
-            )
-
-            DefaultButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 32.dp),
-                text = "REGISTRARSE",
-                onClick = { }
-            )
-        }
-    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
