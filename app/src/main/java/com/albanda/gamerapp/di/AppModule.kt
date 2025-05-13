e3b22a5b946a517c4b0fbaac793d205c2e6dbd03
Albert <afiguerola@gaddexapp.com>
2025-05-13 21:52:40 +0200

    Integrate Firebase database

--- app/src/main/java/com/albanda/gamerapp/di/AppModule.kt
+++ app/src/main/java/com/albanda/gamerapp/di/AppModule.kt
@@ -11,6 +11,7 @@ import com.albanda.gamerapp.domain.usecase.auth.Login
 import com.albanda.gamerapp.domain.usecase.auth.Logout
 import com.albanda.gamerapp.domain.usecase.auth.Signup
 import com.albanda.gamerapp.domain.usecase.user.CreateUser
+import com.albanda.gamerapp.domain.usecase.user.GetUseById
 import com.albanda.gamerapp.domain.usecase.user.UserUseCases
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
@@ -50,6 +51,7 @@ object AppModule {
 
     @Provides
     fun provideUserUseCases(userRepository: UserRepository) = UserUseCases (
-        createUser = CreateUser(userRepository)
+        createUser = CreateUser(userRepository),
+        getUseById = GetUseById(userRepository)
     )
 }
--- app/src/main/java/com/albanda/gamerapp/domain/repository/UserRepository.kt
+++ app/src/main/java/com/albanda/gamerapp/domain/repository/UserRepository.kt
@@ -2,9 +2,12 @@ package com.albanda.gamerapp.domain.repository
 
 import com.albanda.gamerapp.domain.model.Response
 import com.albanda.gamerapp.domain.model.User
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
 
     suspend fun createUser(user: User): Response<Boolean>
+
+    fun getUserById(userId: String): Flow<User>
 
 }
