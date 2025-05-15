334c90f6f35724a14b97f9c1e9876c0f7591a1b8
Albert <afiguerola@gaddexapp.com>
2025-05-15 19:29:32 +0200

    Refactor code

--- app/src/main/java/com/albanda/gamerapp/presentation/screens/signup/components/CardForm.kt
+++ app/src/main/java/com/albanda/gamerapp/presentation/screens/signup/components/CardForm.kt
@@ -1,10 +1,6 @@
 package com.albanda.gamerapp.presentation.screens.signup.components
 
-import android.util.Log
-import android.widget.Toast
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -14,43 +10,34 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
 import com.albanda.gamerapp.R
-import com.albanda.gamerapp.domain.model.Response
 import com.albanda.gamerapp.presentation.components.DefaultButton
 import com.albanda.gamerapp.presentation.components.DefaultTextFiled
-import com.albanda.gamerapp.presentation.navigation.AppScreen
 import com.albanda.gamerapp.presentation.screens.signup.SignupViewModel
 import com.albanda.gamerapp.presentation.ui.theme.DarkGray500
 
 @Composable
 fun CardForm(
-    signupViewModel: SignupViewModel = hiltViewModel(),
-    navHostController: NavHostController
+    signupViewModel: SignupViewModel = hiltViewModel()
 ) {
 
-    val signupFlow = signupViewModel.signupFlow.collectAsState()
+    val state = signupViewModel.state
 
     Card(
         modifier = Modifier
@@ -81,22 +68,22 @@ fun CardForm(
 
             DefaultTextFiled(
                 modifier = Modifier.padding(top = 16.dp),
-                value = signupViewModel.username.value,
-                onValueChange = { signupViewModel.username.value = it },
+                value = state.username,
+                onValueChange = { signupViewModel.onUsernameInput(it) },
                 label = "Nombre de usuario",
                 leadingIcon = Icons.Default.Person,
-                errorMsg = signupViewModel.usernameErrMsg.value,
+                errorMsg = signupViewModel.usernameErrMsg,
                 validateField = { signupViewModel.validateUsername() }
             )
 
             DefaultTextFiled(
                 modifier = Modifier.padding(top = 0.dp),
-                value = signupViewModel.email.value,
-                onValueChange = { signupViewModel.email.value = it },
+                value = state.email,
+                onValueChange = { signupViewModel.onEmailInput(it) },
                 label = "Correo electrónico",
                 leadingIcon = Icons.Default.Email,
                 keyboardType = KeyboardType.Email,
-                errorMsg = signupViewModel.emailErrMsg.value,
+                errorMsg = signupViewModel.emailErrMsg,
                 validateField = { signupViewModel.validateEmail() }
             )
 
@@ -119,27 +106,27 @@ fun CardForm(
 
             DefaultTextFiled(
                 modifier = Modifier.padding(top = 0.dp),
-                value = signupViewModel.password.value,
-                onValueChange = { signupViewModel.password.value = it },
+                value = state.password,
+                onValueChange = { signupViewModel.onPasswordInput(it) },
                 label = "Password",
                 leadingIcon = Icons.Default.Lock,
                 trailingIcon = trailing,
                 hideText = hideText,
                 keyboardType = KeyboardType.Password,
-                errorMsg = signupViewModel.passwordErrMsg.value,
+                errorMsg = signupViewModel.passwordErrMsg,
                 validateField = { signupViewModel.validatePassword() }
             )
 
             DefaultTextFiled(
                 modifier = Modifier.padding(top = 0.dp),
-                value = signupViewModel.confirmPassword.value,
-                onValueChange = { signupViewModel.confirmPassword.value = it },
+                value = state.confirmPassword,
+                onValueChange = { signupViewModel.onConfirmPasswordInput(it) },
                 label = "Password",
                 leadingIcon = Icons.Outlined.Lock,
                 trailingIcon = trailing,
                 hideText = hideText,
                 keyboardType = KeyboardType.Password,
-                errorMsg = signupViewModel.confirmPasswordErrMsg.value,
+                errorMsg = signupViewModel.confirmPasswordErrMsg,
                 validateField = { signupViewModel.validateConfirmPassword() }
             )
 
@@ -154,35 +141,4 @@ fun CardForm(
         }
     }
 
-    signupFlow.value.let { state ->
-        when (state) {
-            Response.Loading -> {
-                Log.i("CardForm", "Loading")
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    CircularProgressIndicator()
-                }
-            }
-
-            is Response.Success<*> -> {
-                LaunchedEffect(Unit) {
-                    signupViewModel.createUser()
-                    navHostController.popBackStack(AppScreen.Login.route, inclusive = true)
-                    navHostController.navigate(route = AppScreen.Profile.route)
-                }
-            }
-
-            is Response.Failure<*> -> {
-                Toast.makeText(
-                    LocalContext.current,
-                    state.exception?.message ?: "Error desconocido",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-
-            null -> {}
-        }
-    }
 }
