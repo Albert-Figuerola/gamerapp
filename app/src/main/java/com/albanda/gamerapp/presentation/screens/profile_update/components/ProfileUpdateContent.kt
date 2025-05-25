dd0a5671e5d400a70241c99c4084cc71a5063952
Albert <afiguerola@gaddexapp.com>
2025-05-25 11:09:27 +0200

    Created flow to take a picture or choose from gallery

--- app/src/main/java/com/albanda/gamerapp/presentation/screens/profile_update/components/ProfileUpdateContent.kt
+++ app/src/main/java/com/albanda/gamerapp/presentation/screens/profile_update/components/ProfileUpdateContent.kt
@@ -1,5 +1,7 @@
 package com.albanda.gamerapp.presentation.screens.profile_update.components
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,9 +13,11 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.albanda.gamerapp.presentation.screens.profile_update.ProfileUpdateViewModel
 
 @Composable
-fun ProfileEditContent() {
+fun ProfileUpdateContent() {
 
     Column(
         modifier = Modifier
