4b0903d8f5941441f641694b2c83a557e65ba538
Albert <afiguerola@gaddexapp.com>
2025-08-05 22:03:29 +0200

    Improving bottom bar navigation.

--- app/src/main/java/com/albanda/gamerapp/presentation/navigation/AuthNavGraph.kt
+++ app/src/main/java/com/albanda/gamerapp/presentation/navigation/AuthNavGraph.kt
@@ -21,3 +21,13 @@ fun NavGraphBuilder.authNavGraph(navHostController: NavHostController) {
         }
     }
 }
+
+sealed class AuthScreen(val route: String) {
+    object Login: AuthScreen("login")
+    object Signup: AuthScreen("signup")
+
+    object Profile: AuthScreen("profile")
+    object ProfileEdit: AuthScreen("profile/edit/{user}") {
+        fun passUser(user: String) = "profile/edit/$user"
+    }
+}
