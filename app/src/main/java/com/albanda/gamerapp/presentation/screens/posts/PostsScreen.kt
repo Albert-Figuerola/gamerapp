963fda49b0d6725c96de626ee2a90ef7869a6eae
Albert <afiguerola@gaddexapp.com>
2025-08-18 22:11:13 +0200

    Created posts card screen

--- app/src/main/java/com/albanda/gamerapp/presentation/screens/posts/PostsScreen.kt
+++ app/src/main/java/com/albanda/gamerapp/presentation/screens/posts/PostsScreen.kt
@@ -2,21 +2,17 @@ package com.albanda.gamerapp.presentation.screens.posts
 
 import android.annotation.SuppressLint
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
 import com.albanda.gamerapp.presentation.screens.posts.components.GetPosts
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun PostsScreen(
-    navHostController: NavHostController,
     getPostsViewModel: GetPostsViewModel = hiltViewModel()
 ) {
     Scaffold (
         content = {
-            Text(text = "PostScreen")
             GetPosts(getPostsViewModel)
         }
     )
